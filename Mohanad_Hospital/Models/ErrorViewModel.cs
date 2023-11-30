a62b53ab196d4b7228f688a336a8ade117440e9c
Mohanad <Mohanadflorra@gmail.com>
2023-11-30 17:40:43 +0300

    Add project files.

---
+++ Mohanad_Hospital/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mohanad_Hospital.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
