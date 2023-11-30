062052798e32717ee7c79c96722df971491d6846
Mohanad <Mohanadflorra@gmail.com>
2023-11-30 19:24:22 +0300

    Added Datatbase

---
+++ Mohanad_Hospital/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mohanad_Hospital.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+        }
+    }
+}
